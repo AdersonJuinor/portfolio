a2c53e26f348037631f2c7b952866f642d140d85
naika <adersonjunior804@gmail.com>
2024-10-29 13:44:38 -0300

    envio

---
+++ menu.js
@@ -0,0 +1,17 @@
+let btnMenu = document.getElementById('abrir-menu')
+
+let menu = document.getElementById('menu-mobile')
+
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu-mobile')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu-mobile')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu-mobile')
+})
